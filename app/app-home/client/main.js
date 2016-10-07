ff64c3af387decc3fe535e881837b98033119423
Meksavanh <ventialaw@hotmail.fr>
2016-10-08 01:07:10 +0200

    Création des templates

---
+++ app/app-home/client/main.js
@@ -0,0 +1,15 @@
+import './main.html';
+
+Template.home.onCreated(function helloOnCreated() {
+
+});
+
+Template.home.helpers({
+
+});
+
+Template.home.events({
+  'click button'(event, instance) {
+
+  },
+});
